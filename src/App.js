8051f813a97cb9e9c9f44d24fe25a412ff85de7c
Scott Bergler <commandinghands@gmail.com>
2020-09-01 10:10:50 -0700

    Install node modules.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Month from './Month';
 
 function App() {
+  const now = new Date();
+
+  console.log(now);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="App">
+      <Month month="January" />
+    </main>
   );
 }
 
